e74b4e5bf6706178119678b0432986e131f76cd3
Eduardo Brnadao Junior <eduardobrandaojunior@gmail.com>
2020-12-01 09:28:21 -0300

    Iclusão dos Pedidos

--- AgileFood/AgileFood/Controllers/PedidosController.cs
+++ AgileFood/AgileFood/Controllers/PedidosController.cs
@@ -11,28 +11,41 @@ namespace AgileFood.Controllers
     public class PedidosController : Controller
     {
         // GET: Pedidos
+        public ActionResult ConsultarFuncionario()
+        {
+            
+            CarregarFuncionarios();
+            return View();
+        }
+        public ActionResult CarregarFornecedores()
+        {
+            MostrarFornecedores();
+
+            return View();
+        }
         public ActionResult Pedidos(string fornecedor)
         {
-
-           
-            CarregarFornecedores();
-
-
+            ConsultarProdutos(fornecedor);
 
             return View();
         }
 
-        public ActionResult Produtos(string fornecedor)
-        {
-            ConsultarProdutos(fornecedor);
+      
 
-            return View("Pedidos");
+        public ActionResult AdicionarItens(string quantidade)
+        {
+            if (quantidade != "0")
+            {
+                
+            }
+
+            return View("CarregarFornecedores");
         }
 
-        public void CarregarFornecedores()
+        public void MostrarFornecedores()
         {
             FornecedorDAO objDao = new FornecedorDAO();
-            List<tb_fornecedor> lst = objDao.ConsultarFornecedores();
+            List<tb_fornecedor> lst = objDao.ConsultarFornecedores().Where(forn =>forn.status_fornecedor == 1).ToList();
 
 
             ViewBag.LstFornecedores = lst;
@@ -50,7 +63,7 @@ namespace AgileFood.Controllers
                 ProdutoDAO objDao = new ProdutoDAO();
 
 
-                List<tb_produto> lst = objDao.ConsultarProduto().Where(prod => prod.id_fornecedor == idFornecedor).ToList();
+                List<tb_produto> lst = objDao.ConsultarProduto().Where(prod => prod.id_fornecedor == idFornecedor && prod.status_produto == 1 ).ToList();
 
 
                 ViewBag.LstProdutos = lst;
@@ -58,5 +71,46 @@ namespace AgileFood.Controllers
             
 
         }
+        public void CarregarFuncionarios()
+        {
+            FuncionarioDAO objDao = new FuncionarioDAO();
+            List<tb_funcionario> lst = objDao.ConsultarFuncionario();
+
+
+            ViewBag.LstFuncionario = lst;
+        }
+        //public void RegistrarPedido()
+        //{
+        //    if (Nome.Trim() == "" || telefone.Trim() == "" || endereco.Trim() == "")
+        //    {
+        //        ViewBag.ret = 0;
+        //        ViewBag.msg = Mensagens.Msg.MensagemCampoObg;
+        //    }
+        //    else
+        //    {
+
+        //        tb_pedidos objPrdido = new tb_pedidos();
+        //        FornecedorDAO ObjDao = new FornecedorDAO();
+
+        //        objFornecedor.nome_fornecedor = Nome;
+        //        objFornecedor.telefone_fornecedor = telefone;
+        //        objFornecedor.endereco_fornecedor = endereco;
+        //        objFornecedor.status_fornecedor = 1;
+
+        //        try
+        //        {
+        //            ObjDao.InserirFornecedor(objFornecedor);
+        //            ViewBag.Ret = 1;
+        //            ViewBag.Msg = Mensagens.Msg.MsgSucesso;
+        //        }
+        //        catch (Exception)
+        //        {
+
+        //            ViewBag.Ret = -1;
+        //            ViewBag.Msg = Mensagens.Msg.MsgErro;
+        //        }
+        //    }
+        //    MontarTitulo(1);
+        //}
     }
 }
---
+++ AgileFood/DAO/PedidoDAO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class PedidoDAO
+    {
+
+        public void InserirProduto(tb_produto ObjProd)
+        {
+            AgileFoodEntities objBanco = new AgileFoodEntities();
+
+            objBanco.tb_produto.Add(ObjProd);
+
+            objBanco.SaveChanges();
+        }
+
+        public void AlterarProduto(tb_produto objProdAtualizado)
+        {
+            AgileFoodEntities objBanco = new AgileFoodEntities();
+
+            tb_produto objResgate = objBanco.tb_produto.Where(prod => prod.id_produto == objProdAtualizado.id_produto).FirstOrDefault();
+
+            objResgate.id_produto = objProdAtualizado.id_produto;
+            objResgate.nome_produto = objProdAtualizado.nome_produto;
+            objResgate.codigo_produto = objProdAtualizado.codigo_produto;
+            objResgate.preco_produto = objProdAtualizado.preco_produto;
+            objResgate.status_produto = objProdAtualizado.status_produto;
+
+            objBanco.SaveChanges();
+        }
+
+        public List<tb_produto> ConsultarProduto()
+        {
+            AgileFoodEntities objBanco = new AgileFoodEntities();
+            List<tb_produto> LstRetorno = objBanco.tb_produto.ToList();
+
+            return LstRetorno;
+        }
+    }
+}
